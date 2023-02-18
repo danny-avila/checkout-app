cb350a55e09fe1c5bec2e73b62eef6c3a7b52a76
Daniel Avila <messagedaniel@protonmail.com>
2023-02-18 12:04:06 -0500

    initial commit

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const routes = require('./routes');
+const path = require('path');
+const app = express();
+const port = 3000;
+const projectPath = path.join(__dirname, '..');
+
+app.use(express.json());
+app.use(express.static(path.join(projectPath, 'public')));
+
+app.get('/', function (req, res) {
+  console.log(path.join(projectPath, 'public', 'index.html'));
+  res.sendFile(path.join(projectPath, 'public', 'index.html'));
+});
+
+// app.use('/words', routes.words);
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+import React, { useEffect, useState } from 'react';
+
+const App = () => {
+};
+
+export default App;
